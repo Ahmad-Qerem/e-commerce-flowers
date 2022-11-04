0dc4d166e9fd31ed0bcfe5e0f40d953ca5863fad
EmadNazzal <emadnazzal95@gmail.com>
2022-11-04 12:11:59 +0200

    Added the New Section

--- src/components/HomePage/Cards/NewSection/NewSection.js
+++ src/components/HomePage/Cards/NewSection/NewSection.js
@@ -4,13 +4,24 @@ import "./NewSection.css";
 import Data from "../../../../Data.json";
 
 // const jsData = JSON.parse(flowerlist);
-console.log(Data);
+const flowerData = Data.flowerlist.slice(0, 4);
+console.log(flowerData);
 const NewSection = () => {
   return (
     <React.Fragment>
       <HeadingTitle title="New" />
 
-      <div className="newSection"></div>
+      <div className="newSection">
+        {flowerData.map((flower) => {
+          return (
+            <div key={flower.productId}>
+              <img src={flower.photo} alt={flower.category} />
+              <h3>{flower.name}</h3>
+              <p>{flower.price} $</p>
+            </div>
+          );
+        })}
+      </div>
     </React.Fragment>
   );
 };
