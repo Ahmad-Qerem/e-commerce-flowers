9c468d3d80a9ba49371efe2cf946e61e3d69edc2
EmadNazzal <emadnazzal95@gmail.com>
2022-11-05 10:31:12 +0200

    added the btns

--- src/components/HomePage/Cards/ReusableCard/ReusableCard.js
+++ src/components/HomePage/Cards/ReusableCard/ReusableCard.js
@@ -7,8 +7,12 @@ const ReusableCard = (props) => {
     <>
       {flowerData.map((flower) => {
         return (
-          <div key={flower.productId}>
+          <div key={flower.productId} className="cardFlower">
             <img src={flower.photo} alt={flower.category} />
+            <div className="cartAndHeart">
+              <i class="fa-regular fa-heart"></i>
+              <i class="fa-solid fa-cart-shopping"></i>
+            </div>
             <h3>{flower.name}</h3>
             <p>{flower.price} $</p>
           </div>
