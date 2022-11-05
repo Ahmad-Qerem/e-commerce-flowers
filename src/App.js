a3396f9b5e88d419f7c3af94acbd14685686986b
EmadNazzal <emadnazzal95@gmail.com>
2022-11-05 09:17:33 +0200

    merged into all components file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from "react";
 import "./App.css";
-import NewSection from "./components/HomePage/Cards/NewSection/NewSection";
-import ReleventSection from "./components/HomePage/Cards/ReleventSection/ReleventSection";
-import Footer from "./components/HomePage/Footer/Footer";
-import MainBanner from "./components/HomePage/MainBanner/MainBanner";
-import SecondaryBanners from "./components/HomePage/SecondaryBanners/SecondaryBanners";
-import LogoAndIcons from "./components/HomePage/TitleAndNav/LogoAndIcons/LogoAndIcons";
-import NavBar from "./components/HomePage/TitleAndNav/NavBar/NavBar";
+import * as all from "./components/AllComponents";
 
 function App() {
   return (
     <>
-      <div className="Container">
-        <LogoAndIcons />
-        <NavBar />
-        <MainBanner
-          title="40 % OFF"
-          des="Best Deals This Week. Fresh Flowers, Plants and Gifts !"
-        />
-        <SecondaryBanners />
-        <NewSection />
-        <ReleventSection />
-      </div>
-      <Footer />
+      <all.HomePage />
     </>
   );
 }
